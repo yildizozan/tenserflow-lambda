3553d28282d6281d26278a4258c1418d3d2b4035
Ozan Yıldız <developer@yildizozan.com>
2019-11-14 08:58:27 +0300

    test

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ quotes = [
           "Muhammad Waseem"),
     Quote(
         "Progress is possible only if we train ourselves to think about programs without thinking of them as pieces of executable code. ",
-        "Edsger W. Dijkstra")
+        "Edsger W. Dijkstra.")
 ]
 
 app = Flask(__name__)
